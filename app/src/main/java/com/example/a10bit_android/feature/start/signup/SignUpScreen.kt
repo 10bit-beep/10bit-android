7a73108b43d09eab625ce8e6c70382b093020bda
rtxic011 <kyukami7@gmail.com>
2025-10-12 02:23:21 +0900

    add SignUpButton

--- app/src/main/java/com/example/a10bit_android/feature/start/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/a10bit_android/feature/start/signup/SignUpScreen.kt
@@ -9,12 +9,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.a10bit_android.R
+import com.example.a10bit_android.ui.component.textfield.SignUpErrorType
+import com.example.a10bit_android.ui.component.textfield.SignUpTextField
 import com.example.a10bit_android.ui.theme.AuthBackground
 
 @Composable
@@ -40,6 +46,53 @@ fun SignUpScreen (
             )
 
             Spacer(modifier = Modifier .height(52.dp))
+
+            var username by remember { mutableStateOf("") }
+            var password by remember { mutableStateOf("") }
+            var studentId by remember { mutableStateOf("") }
+            var email by remember { mutableStateOf("") }
+
+            var usernameErrorType by remember { mutableStateOf(SignUpErrorType.NONE) }
+            var passwordErrorType by remember { mutableStateOf(SignUpErrorType.NONE) }
+            var studentIdErrorType by remember { mutableStateOf(SignUpErrorType.NONE) }
+            var emailErrorType by remember { mutableStateOf(SignUpErrorType.NONE) }
+
+            SignUpTextField( "아이디",
+                value = username,
+                onValueChange = { username = it },
+                placeholder = "아이디를 입력해주세요.",
+                errorType = usernameErrorType
+            )
+
+            Spacer(modifier = Modifier .height(20.dp))
+
+            SignUpTextField(
+                "비밀번호",
+                value = password,
+                onValueChange = { password = it },
+                placeholder = "비밀번호를 입력해주세요.",
+                errorType = passwordErrorType,
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            SignUpTextField(
+                "학번",
+                value = studentId,
+                onValueChange = { studentId = it },
+                placeholder = "학번을 입력해주세요.",
+                errorType = studentIdErrorType
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            SignUpTextField(
+                "이메일",
+                value = email,
+                onValueChange = { email = it },
+                placeholder = "이메일을 입력해주세요.",
+                errorType = emailErrorType,
+            )
 
 
 
---
+++ app/src/main/java/com/example/a10bit_android/ui/component/button/SignUpButton.kt
@@ -0,0 +1,39 @@
+package com.example.a10bit_android.ui.component.button
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.a10bit_android.ui.theme.mainColor
+
+@Composable
+fun SignUpButton(
+    buttonname: String,
+    onclick: () -> Unit
+) {
+    Button(
+        onClick = onclick,
+        modifier = Modifier
+            .width(323.dp)
+            .height(52.dp),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = mainColor,
+            contentColor = Color.White
+        )
+    ) {
+        Text(
+            text = buttonname,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium
+        )
+    }
+}
